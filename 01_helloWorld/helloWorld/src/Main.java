8e9169da512ca1682ed5e5419743358bf30ba0cb
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-02-24 15:40:37 -0500

    creation of a java basic project

---
+++ 01_helloWorld/helloWorld/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("this is a basic project created with JDK 17.0.6 and its only function is to print the message 'hello world'.");
+    }
+}
