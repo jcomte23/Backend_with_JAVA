19157a59e5dfeebd48b0e2828d259d4bc61b3dcd
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-02-24 16:33:50 -0500

    comment entry in english

--- 01_helloWorld/helloWorld/src/Main.java
+++ 01_helloWorld/helloWorld/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("this is a basic project created with JDK 17.0.6 and its only function is to print the message 'hello world'.");
+        // este es un proyecto básico creado con JDK 17.0.6 y su única función es imprimir el mensaje 'hola mundo'.
     }
 }
